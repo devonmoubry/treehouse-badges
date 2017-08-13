0fb24e7912b394d06abfa5381b3fd31b58c1abe5
Devon Moubry <devon@moubry.com>
2017-08-13 15:18:08 -0500

    require module

---
+++ profile.js
@@ -0,0 +1,47 @@
+const https = require('https');
+const http = require('http');
+//Function to print message to console
+
+// print error messages
+function printError(error) {
+  console.error(error.message);
+}
+
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+  console.log(message);
+}
+
+function get(username) {
+  try {
+    // connect to api url(https://teamtreehouse.com/devonmoubry.json)
+    const request = https.get('https://teamtreehouse.com/' + username + '.json', response => {
+      if (response.statusCode === 200) {
+        let body = "";
+        // read the data
+        response.on('data', (data) => {
+          body += data.toString();
+        })
+        response.on('end', () => {
+          try {
+            // parse the data
+            const profile = JSON.parse(body);
+            // print the data
+            printMessage(username, profile.badges.length, profile.points.JavaScript)
+          } catch (e) {
+            printError(e);
+          }
+        })
+      } else {
+        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }
+    })
+    request.on('error', printError)
+  } catch (e) {
+    printError(e);
+  }
+}
+
+module.exports.get = get;
